93866c7e69e68a5a08193939bed747e701fdeb5c
Anusha Hegde <anusha.hegde@nirmata.com>
2024-04-12 11:07:43 +0200

    Update install-tf.sh

--- scripts/install-tf.sh
+++ scripts/install-tf.sh
@@ -22,6 +22,7 @@ run_terraform_commands() {
     terraform plan -out tfplan.binary
     # Store in JSON
     terraform show -json tfplan.binary | jq > payload.json
+    cat payload.json
     echo "Terraform commands executed successfully."
 }
 
