911744bc4cff1516a1db115720333173dcc0dbe0
Anusha Hegde <anusha.hegde@nirmata.com>
2024-04-12 10:26:14 +0200

    Add script for generating payload

---
+++ scripts/install-tf.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Function to install Terraform
+install_terraform() {
+    echo "Installing Terraform..."
+    # Download Terraform binary
+    wget https://releases.hashicorp.com/terraform/0.15.0/terraform_0.15.0_linux_amd64.zip
+    # Unzip the downloaded file
+    unzip terraform_0.15.0_linux_amd64.zip
+    # Move Terraform binary to /usr/local/bin
+    sudo mv terraform /usr/local/bin/
+    echo "Terraform installed successfully."
+}
+
+# Function to run Terraform commands
+run_terraform_commands() {
+    echo "Running Terraform commands..."
+    # Initialize Terraform in the directory where your configuration files are located
+    terraform init
+    # Plan the changes Terraform will make
+    terraform plan -out tfplan.binary
+    # Store in JSON
+    terraform show -json tfplan.binary | jq > payload.json
+    echo "Terraform commands executed successfully."
+}
+
+# Check if Terraform is already installed
+if command -v terraform &> /dev/null; then
+    echo "Terraform is already installed."
+    run_terraform_commands
+else
+    install_terraform
+    run_terraform_commands
+fi
+
